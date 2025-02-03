ef799f34beb3669817660d6589420a17b8a34e20
arpan <arpanchatterjeeg@gmail.com>
2025-02-03 18:23:32 -0500

    Initial setup of the project with tailwindcss and expo

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar"; 
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+import { Link } from "expo-router"; 
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">hello index</Text>
+      <StatusBar style="auto" />  
+      <Link href="/profile" style = {{color: "red"}}>Profile</Link>
+    </View>
+  );
+}
+
