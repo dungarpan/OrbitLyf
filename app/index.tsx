a5f12ccabea306dee7d97302aab6aaa0b658ffd3
arpan <arpanchatterjeeg@gmail.com>
2025-02-03 19:11:02 -0500

    added personal fonts to layout.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold">hello index</Text>
+      <Text className="text-2xl font-pblack">hello index</Text>
       <StatusBar style="auto" />  
       <Link href="/profile" style = {{color: "red"}}>Profile</Link>
     </View>
